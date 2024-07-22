01cc9c1c3fc4459e420f76758e9ae9c849b86b1d
farildev <memmedovfaril@gmail.com>
2024-07-22 22:27:50 +0400

    feat: Authentication flow created between pages

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,5 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom";
+import { auth } from "@/config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth"
 const AppLayout = () => {
+  const [user, isLoading] = useAuthState(auth);
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(!user){
+    return <Navigate to="/login" replace />
+  }
   return (
     <main>
       <Outlet />
--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,5 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom";
+import { auth } from "@/config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth"
 const AuthLayout = () => {
+  const [user, isLoading] = useAuthState(auth);
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+  
+  if(user){
+    return <Navigate to='/' replace />
+  }
+
   return (
     <main>
       <Outlet />
